e79465dfd1e8728bfab0e32418cb5fd7d488752e
Otto <otto.rinaldo@gmail.com>
2023-12-02 19:12:30 +0100

    add AoC 1 and AoC 2

---
+++ aoc-1/Program.cs
@@ -0,0 +1,88 @@
+﻿using System.Data;
+using inputSpace;
+
+// AoC 1.2
+
+int total = 0;
+int[] numberArray = Enumerable.Range(1, 9).ToArray();
+string[] stringifiedNumbers = numberArray.Select(n => n.ToString()).ToArray();
+
+string[] combined = stringifiedNumbers.Concat(InputClass.inputWordNumbers).ToArray();
+
+
+string[] splitInput = InputClass.inputString.Split("\n");
+// string[] splitInput = ["fasfj545"];
+
+
+foreach (var line in splitInput)
+{
+    int firstNumber = getFirstNumber(combined, line);
+    int lastNumber = getLastNumber(combined, line);
+
+
+
+    _ = int.TryParse(firstNumber.ToString() + lastNumber.ToString(), out int lastInt);
+    total += lastInt;
+    Console.WriteLine(total);
+}
+    Console.WriteLine(total);
+
+static int getFirstNumber(string[] combined, string line)
+{
+    var firstIndex = int.MaxValue;
+    var firstNumber = "";
+    foreach (var number in combined)
+    {
+        var index = 0;
+        index = line.IndexOf(number) > -1 ? line.IndexOf(number) : int.MaxValue;
+
+        var isFirst = index < firstIndex;
+        firstIndex = isFirst ? index : firstIndex;
+        firstNumber = isFirst ? number : firstNumber;
+    }
+
+    _ = int.TryParse(firstNumber.ToString(), out int firstInt);
+    return firstInt == 0 ? Array.IndexOf(InputClass.inputWordNumbers, firstNumber) + 1 : firstInt;
+}
+
+static int getLastNumber(string[] combined, string line)
+{
+    var lastIndex = int.MinValue;
+    var lastNumber = "";
+    foreach (var number in combined)
+    {
+        var index = 0;
+        index = line.LastIndexOf(number) > -1 ? line.LastIndexOf(number) : int.MinValue;
+
+        var isLast = index > lastIndex;
+        lastIndex = isLast ? index : lastIndex;
+        lastNumber = isLast ? number : lastNumber;
+    }
+
+    _ = int.TryParse(lastNumber.ToString(), out int lastInt);
+    return lastInt == 0 ? Array.IndexOf(InputClass.inputWordNumbers, lastNumber) + 1 : lastInt;
+}
+
+
+// AoC 1.1 (broken)
+// foreach (var line in splitInput)
+// {
+//     var firstInt = 0;
+//     foreach (var ch in line.ToCharArray())
+//     {
+//         var foundFirst = int.TryParse(ch.ToString(), out firstInt);
+//         if (foundFirst)
+//             break;
+
+//     }
+//     var lastInt = 0;
+//     foreach (var ch in line.Reverse())
+//     {
+//         var foundLast = int.TryParse(ch.ToString(), out lastInt);
+//         if (foundLast)
+//             break;
+//     }
+//     Console.Write($"{total} + {firstInt}{lastInt} = ");
+//     total += int.Parse($"{firstInt}{lastInt}");
+// }
+
---
+++ aoc-2/Program.cs
@@ -0,0 +1,62 @@
+﻿
+Console.WriteLine("         øttø");
+var COLORS = Enum.GetNames(typeof(ValidationEnum));
+
+string balls = Input.Balls;
+string[] games = balls.Split("\n");
+var validBallNumbers = new Dictionary<string, int>
+{
+    { "red", 12 },
+    { "green", 13 },
+    { "blue", 14 }
+};
+
+List<int> sumOfProducts = [];
+foreach (var game in games)
+{
+    // Game
+    Console.WriteLine(game);
+    string[] subStrings = game.Split([':', ';']);
+    var gameNumber = subStrings[0];
+    _ = int.TryParse(gameNumber.Replace("Game", "").Trim(), out int gameId);
+    var gameOutcome = subStrings.Skip(1).ToArray();
+    var minRedBalls = int.MinValue;
+    var minGreenBalls = int.MinValue;
+    var minBlueBalls = int.MinValue;
+    foreach (string draw in gameOutcome)
+    {
+        // One draw
+        var ballsInDraw = draw.Split(',');
+        foreach (var oneColorBalls in ballsInDraw)
+        {
+            // One color drawn
+            var extractedColor = COLORS.First(oneColorBalls.Contains);
+            _ = int.TryParse(oneColorBalls.Replace(extractedColor, ""), out int numberOfColoredBall);
+            validBallNumbers.TryGetValue(extractedColor, out int validNumberOfBalls);
+
+            switch (extractedColor)
+            {
+                case "red":
+                    minRedBalls = numberOfColoredBall > minRedBalls ? numberOfColoredBall : minRedBalls;
+                    break;
+                case "green":
+                    minGreenBalls = numberOfColoredBall > minGreenBalls ? numberOfColoredBall : minGreenBalls;
+                    break;
+                case "blue":
+                    minBlueBalls = numberOfColoredBall > minBlueBalls ? numberOfColoredBall : minBlueBalls;
+                    break;
+            }
+        }
+    }
+    sumOfProducts.Add(minRedBalls * minGreenBalls * minBlueBalls);
+}
+
+Console.WriteLine($"Sum of products: {sumOfProducts.Sum()}");
+
+
+enum ValidationEnum : int
+{
+    red = 12,
+    green = 13,
+    blue = 14,
+};
